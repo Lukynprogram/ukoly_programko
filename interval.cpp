3d5036d592a16d9030781ea21f73b678bff16041
Lukyn_Program_Cichnova <75174573+Lukynprogram@users.noreply.github.com>
2021-03-28 06:47:01 -0100

    Update interval.cpp

--- interval.cpp
+++ interval.cpp
@@ -5,30 +5,32 @@ int vstup(){
 std::cout << "Vlozte zacatek intervalu prosim <1, 10>: ";   //vstup pro a
 std::cin >> a;
 
-std::cout << "vlozte konec intervalu prosim <50, 60>";  //vtup pro b
+std::cout << "vlozte konec intervalu prosim <50, 60>: ";  //vtup pro b
 std::cin >> b;
 
 std::cout << "Zadejte cislo, ktere chcete vyhodnotit: ";    //vstup pro c
 std::cin >> c;
+return 0; 
 }
 
 int vyhodnoceni(){
     if(a <= 10 && a >= 1){
         if(b <= 60 && b >= 50){
             if(c <= b && c >= a){
-                std::cout << "Lezi v intervalu";
+                std::cout << "\nLezi v intervalu";
             }
             else{
-                std::cout << "Nelezi v intervalu";
+                std::cout << " \nNelezi v intervalu";
             }
         }
         else{
-        	std::cout << "Zadal jsi spatne konec intervalu";
+        	std::cout << "\nZadal jsi spatne konec intervalu";
 		}
-    }
+    } 
     else{
-        std::cout << "Zadal jsi spatne zacatek intervalu";
+        std::cout << "\nZadal jsi spatne zacatek intervalu";
     }
+    return 0;
 }
 
 int main(){
