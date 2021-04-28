d70f7ab7fdba68a7229a02b6402e00af8d7e9fcf
Lukyn_Program_Cichnova <75174573+Lukynprogram@users.noreply.github.com>
2021-04-28 13:59:59 +0200

    Update whileloopdruha.cpp

--- whileloopdruha.cpp
+++ whileloopdruha.cpp
@@ -2,23 +2,24 @@
 
 int main()
 {
-  int cislo;
-  int kladna = 0;
-  int zaporna = 0;
-
-  while (cislo != 0) 
-  {
+    int cislo;
+    int kladna = 0;
+    int zaporna = 0;
     printf("Vloz cislo: {exit = 0}: ");
     scanf("%d", &cislo);
-    if (cislo < 0)
+    while (cislo != 0)
     {
-      zaporna = zaporna + 1;
+        if (cislo < 0)
+        {
+            zaporna = zaporna + 1;
+        }
+        else if (cislo >= 1)
+        {
+            kladna = kladna + 1;
+        }
+        printf("Vloz cislo: {exit = 0}: ");
+        scanf("%d", &cislo);
     }
-    else if (cislo >= 1)
-    {
-      kladna = kladna + 1;
-    }
-  }
-  printf("Pocet zapornych cisel je: %d \n", zaporna);
-  printf("Pocet kladnych cisel je: %d\n", kladna);
+    printf("Pocet zapornych cisel je: %d \n", zaporna);
+    printf("Pocet kladnych cisel je: %d\n", kladna);
 }
